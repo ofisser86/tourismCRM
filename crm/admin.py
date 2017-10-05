6f628890c5566cadaad73dcba50d8329fa0c5735
ofisser86 <ofisser86@gmail.com>
2017-10-05 23:07:04 +0800

    add inlines fields

--- crm/admin.py
+++ crm/admin.py
@@ -1,11 +1,17 @@
 from django.contrib import admin
 from .models import  Client, Email, Phone
 # Register your models here.
+
+class PhoneInline(admin.TabularInline):
+    model = Phone
+    extra = 0
+
 class ClientAdmin(admin.ModelAdmin):
-    # list_display = [field.name for field in Client._meta.fields]
-    phone = Phone.objects.filter()
-    print(phone)
+    list_display = [field.name for field in Client._meta.fields]
+    # phone = Phone.objects.filter()
+    # print(phone)
     # list_display = ('first_name', 'last_name', 'phone')
+    inlines = [PhoneInline]
 
     class Meta(object):
         model = Client
